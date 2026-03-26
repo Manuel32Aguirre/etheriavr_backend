91af0317b18915640610ae3be695f3885a52a7b1
Manuel Aguirre <programadorjrsemad@gmail.com>
2026-03-25 20:43:17 -0600

    Limpieza de lista de canciones

--- import.sql
+++ import.sql
@@ -1,13 +1,11 @@
-INSERT INTO artists (id, name) VALUES (3, 'Queen');
-INSERT INTO artists (id, name) VALUES (4, 'Frédéric Chopin');
-INSERT INTO artists (id, name) VALUES (5, 'Michael Jackson');
-INSERT INTO artists (id, name) VALUES (6, 'Dave Brubeck');
-INSERT INTO artists (id, name) VALUES (7, 'Ludwig van Beethoven');
+INSERT INTO artists (id, name) VALUES (1, 'A-ha');
+INSERT INTO artists (id, name) VALUES (2, 'Alex North');
+INSERT INTO artists (id, name) VALUES (3, 'The Beatles');
+INSERT INTO artists (id, name) VALUES (4, 'Elton John');
 
 INSERT INTO songs (musical_genre, musical_key, title, duration, mode, tempo, file_path, artist_id) 
 VALUES 
-('Synth-Pop', 'A', 'Take On Me', 229, 'CANTO', 169, 'songs/bohemian.midi', 3),
-('Clásica', 'Eb', 'Nocturne Op. 9 No. 2', 270, 'PIANO', 60, 'songs/nocturne.midi', 4),
-('Pop', 'F#m', 'Billie Jean', 294, 'CANTO', 117, 'songs/billiejean.midi', 5),
-('Jazz', 'Ebm', 'Take Five', 324, 'PIANO', 174, 'songs/takefive.midi', 6),
-('Clásica', 'Am', 'Test Song', 30, 'PIANO', 120, 'test_song.json', 7);
+('Synth-Pop', 'A', 'Take On Me', 229, 'CANTO', 169, 'songs/bohemian.midi', 1),
+('Rock', 'C', 'Unchained melody', 229, 'PIANO', 169, 'songs/unchainedmelody.json', 2);
+('Rock', 'C', 'Let it be', 229, 'PIANO', 169, 'songs/letitbe.json', 3);
+('Rock', 'Gm', 'Rocketman', 229, 'PIANO', 169, 'songs/rocketman.json', 4);
